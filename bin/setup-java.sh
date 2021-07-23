cebd95744f20222ac11eafc01d5df37b9475729a
Scott Hallock <scott@hallocks.net>
2021-07-23 17:09:18 +0000

    Make Java setup less version-dependent

--- bin/setup-java.sh
+++ bin/setup-java.sh
@@ -4,8 +4,8 @@ curl -L https://corretto.aws/downloads/latest/amazon-corretto-11-x64-linux-jdk.t
 curl -L https://mirror.cogentco.com/pub/apache/maven/maven-3/3.8.1/binaries/apache-maven-3.8.1-bin.tar.gz | gzip -dc | tar -C $HOME/lang/java -xf -
 curl -L https://services.gradle.org/distributions/gradle-7.1.1-bin.zip > $HOME/tmp/gradle-bin.zip
 unzip $HOME/tmp/gradle-bin.zip -d $HOME/lang/java
-ln -s $HOME/lang/java/amazon-corretto-11.0.11.9.1-linux-x64 $HOME/lang/java/jdk
-ln -s $HOME/lang/java/apache-maven-3.8.1 $HOME/lang/java/apache-maven
-ln -s $HOME/lang/java/gradle-7.1.1 $HOME/lang/java/gradle
+ln -s $HOME/lang/java/amazon-corretto-* $HOME/lang/java/jdk
+ln -s $HOME/lang/java/apache-maven-* $HOME/lang/java/apache-maven
+ln -s $HOME/lang/java/gradle-* $HOME/lang/java/gradle
 rm -f $HOME/tmp/gradle-bin.zip
 
