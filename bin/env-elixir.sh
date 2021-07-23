7cddb7e769ab8c40f5f4255f1a83f34c3479700c
Scott Hallock <scott@hallocks.net>
2021-07-23 18:13:44 +0000

    Add setup and environment scripts for Elixir

---
+++ bin/env-elixir.sh
@@ -0,0 +1,3 @@
+export ELIXIR_HOME=$HOME/lang/elixir
+. ~/lang/erlang/otp/activate
+export PATH=$ELIXIR_HOME/bin:$PATH
---
+++ bin/setup-elixir.sh
@@ -0,0 +1,14 @@
+ERLANG_VERSION='24.0.4'
+ELIXIR_VERSION='1.12.2'
+curl -L https://raw.githubusercontent.com/kerl/kerl/master/kerl > /tmp/kerl
+install /tmp/kerl $HOME/bin/kerl
+rm -f /tmp/kerl
+$HOME/bin/kerl build $ERLANG_VERSION $ERLANG_VERSION
+mkdir -p $HOME/lang/erlang
+$HOME/bin/kerl install $ERLANG_VERSION ~/lang/erlang/$ERLANG_VERSION
+ln -s $HOME/lang/erlang/$ERLANG_VERSION $HOME/lang/erlang/otp
+$HOME/bin/kerl cleanup $ERLANG_VERSION
+mkdir -p $HOME/lang/elixir
+curl -L https://github.com/elixir-lang/elixir/releases/download/v$ELIXIR_VERSION/Precompiled.zip > /tmp/elixir.zip
+unzip /tmp/elixir.zip -d $HOME/lang/elixir
+rm -f /tmp/elixir.zip
