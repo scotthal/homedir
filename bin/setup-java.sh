907106b88671b831ab60774e91f8f943f8fa983b
scotthal <scott@hallocks.net>
2021-09-20 07:55:29 -0600

    Update to Java 17

--- bin/setup-java.sh
+++ bin/setup-java.sh
@@ -1,6 +1,6 @@
 mkdir -p $HOME/tmp
 mkdir -p $HOME/lang/java
-curl -L https://corretto.aws/downloads/latest/amazon-corretto-11-x64-linux-jdk.tar.gz | gzip -dc | tar -C $HOME/lang/java -xf -
+curl -L https://corretto.aws/downloads/latest/amazon-corretto-17-x64-linux-jdk.tar.gz | gzip -dc | tar -C $HOME/lang/java -xf -
 curl -L https://mirror.cogentco.com/pub/apache/maven/maven-3/3.8.2/binaries/apache-maven-3.8.2-bin.tar.gz | gzip -dc | tar -C $HOME/lang/java -xf -
 curl -L https://services.gradle.org/distributions/gradle-7.2-bin.zip > $HOME/tmp/gradle-bin.zip
 unzip $HOME/tmp/gradle-bin.zip -d $HOME/lang/java
