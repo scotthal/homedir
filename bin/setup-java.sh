ec893bd7068a8407f52cfb3e77bc4f0ff6d405a1
scotthal <scott@hallocks.net>
2021-11-14 11:42:00 -0700

    setup-java.sh:  Update Gradle version

--- bin/setup-java.sh
+++ bin/setup-java.sh
@@ -2,7 +2,7 @@ mkdir -p $HOME/tmp
 mkdir -p $HOME/lang/java
 curl -L https://corretto.aws/downloads/latest/amazon-corretto-17-x64-linux-jdk.tar.gz | gzip -dc | tar -C $HOME/lang/java -xf -
 curl -L https://dlcdn.apache.org/maven/maven-3/3.8.3/binaries/apache-maven-3.8.3-bin.tar.gz | gzip -dc | tar -C $HOME/lang/java -xf -
-curl -L https://services.gradle.org/distributions/gradle-7.2-bin.zip > $HOME/tmp/gradle-bin.zip
+curl -L https://services.gradle.org/distributions/gradle-7.3-bin.zip > $HOME/tmp/gradle-bin.zip
 unzip $HOME/tmp/gradle-bin.zip -d $HOME/lang/java
 ln -s $HOME/lang/java/amazon-corretto-* $HOME/lang/java/jdk
 ln -s $HOME/lang/java/apache-maven-* $HOME/lang/java/apache-maven
