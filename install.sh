90910f41dd88ffc937bac2c8dcd5407112960b93
scotthal <scott@hallocks.net>
2022-07-01 14:16:30 -0600

    Add a terrible installer script

---
+++ install.sh
@@ -0,0 +1,16 @@
+ENTRIES='
+  .config
+  bin
+  .bash_logout
+  .bash_profile
+  .bashrc
+  .gitconfig
+  .inputrc
+  .tmux.conf
+  .vimrc
+  .xscreensaver
+  .zlogout
+  .zshenv
+  .zshrc
+'
+for entry in $ENTRIES; do cp -a $entry $HOME/$entry; done
