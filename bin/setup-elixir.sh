40cecadd8367b58327f3a7479476d0b8bf7dc31a
Scott Hallock <scott@hallocks.net>
2022-07-22 17:49:54 +0000

    Actual working elixir build

--- bin/setup-elixir.sh
+++ bin/setup-elixir.sh
@@ -9,7 +9,12 @@ mkdir -p $HOME/lang/erlang
 $HOME/bin/kerl install $ERLANG_VERSION ~/lang/erlang/$ERLANG_VERSION
 ln -s $HOME/lang/erlang/$ERLANG_VERSION $HOME/lang/erlang/otp
 $HOME/bin/kerl cleanup $ERLANG_VERSION
+. $HOME/lang/erlang/$ERLANG_VERSION/activate
 mkdir -p $HOME/lang/elixir
-curl -L https://github.com/elixir-lang/elixir/releases/download/v$ELIXIR_VERSION/Precompiled.zip > /tmp/elixir.zip
-unzip /tmp/elixir.zip -d $HOME/lang/elixir
-rm -f /tmp/elixir.zip
+curl -L https://github.com/elixir-lang/elixir/archive/refs/tags/v${ELIXIR_VERSION}.tar.gz | gzip -dc | tar -C /tmp -xf -
+OLD_WD=$PWD
+cd /tmp/elixir-${ELIXIR_VERSION}
+PREFIX=$HOME/lang/elixir make install
+cd $OLD_WD
+rm -rf /tmp/elixir-${ELIXIR_VERSION}
+
