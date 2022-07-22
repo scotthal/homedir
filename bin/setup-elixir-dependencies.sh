c8cc3a010d7aba20c920a2086190f8f58e5314b5
Scott Hallock <scott@hallocks.net>
2022-07-22 16:25:14 +0000

    Setup dependencies for building erlang/elixir

---
+++ bin/setup-elixir-dependencies.sh
@@ -0,0 +1,10 @@
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y update
+apt-get -y install \
+  libwxgtk3.0-gtk3-dev \
+  default-jdk \
+  unixodbc-dev \
+  xsltproc \
+  fop \
+  libxml2-utils
+
