7553adc48eed169e97e03d31cfa6025f43be900e
Scott Hallock <scott@hallocks.net>
2022-07-22 17:03:57 +0000

    Forgot wxwebview

--- bin/setup-elixir-dependencies.sh
+++ bin/setup-elixir-dependencies.sh
@@ -2,6 +2,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -y update
 apt-get -y install \
   libwxgtk3.0-gtk3-dev \
+  libwxgtk-webview3.0-gtk3-dev \
   default-jdk \
   unixodbc-dev \
   xsltproc \
