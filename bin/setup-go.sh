c68b9a24382db305f3c8bc3341e74dcb8caf3b48
Scott Hallock <scott@hallocks.net>
2025-01-09 15:27:33 -0700

    Update to go 1.23.4

--- bin/setup-go.sh
+++ bin/setup-go.sh
@@ -1,3 +1,3 @@
 mkdir -p $HOME/lang
-curl -L https://go.dev/dl/go1.18.3.linux-amd64.tar.gz | gzip -dc | tar -C $HOME/lang -xf -
+curl -L https://go.dev/dl/go1.23.4.linux-amd64.tar.gz | gzip -dc | tar -C $HOME/lang -xf -
 
