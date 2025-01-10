4d0bb675f44e1897232df124cf43947421927fd8
Scott Hallock <scott@hallocks.net>
2025-01-10 11:36:07 -0700

    Add initial neovim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,20 @@
+vim.cmd("set nocp")
+vim.cmd("filetype on")
+vim.cmd("syntax on")
+
+vim.cmd("set noru")
+vim.cmd("set laststatus=0")
+vim.cmd("set sm")
+vim.cmd("set ai")
+vim.cmd("set hlsearch")
+
+vim.cmd("set vb")
+vim.cmd("set bs=\"\"")
+vim.cmd("set viminfo=\"\"")
+
+vim.cmd("set et")
+vim.cmd("set ts=2")
+vim.cmd("set sw=2")
+
+vim.cmd("colorscheme elflord")
+
