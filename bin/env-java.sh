5e5c75a2882a1a61e56e63ca97d578f4e961cc37
Scott Hallock <scott@hallocks.net>
2025-01-10 10:56:28 -0700

    Move Java to ~/.local

--- bin/env-java.sh
+++ bin/env-java.sh
@@ -1,5 +1,5 @@
-export JAVA_HOME=$HOME/lang/java/jdk
-export MAVEN_HOME=$HOME/lang/java/apache-maven
-export GRADLE_HOME=$HOME/lang/java/gradle
+export JAVA_HOME=$HOME/.local/java/jdk
+export MAVEN_HOME=$HOME/.local/java/apache-maven
+export GRADLE_HOME=$HOME/.local/java/gradle
 export PATH=$GRADLE_HOME/bin:$MAVEN_HOME/bin:$JAVA_HOME/bin:$PATH
 
--- bin/setup-java.sh
+++ bin/setup-java.sh
@@ -1,11 +1,11 @@
 mkdir -p $HOME/tmp
-mkdir -p $HOME/lang/java
-curl -L https://corretto.aws/downloads/latest/amazon-corretto-17-x64-linux-jdk.tar.gz | gzip -dc | tar -C $HOME/lang/java -xf -
-curl -L https://dlcdn.apache.org/maven/maven-3/3.9.2/binaries/apache-maven-3.9.2-bin.tar.gz | gzip -dc | tar -C $HOME/lang/java -xf -
+mkdir -p $HOME/.local/java
+curl -L https://corretto.aws/downloads/latest/amazon-corretto-17-x64-linux-jdk.tar.gz | gzip -dc | tar -C $HOME/.local/java -xf -
+curl -L https://dlcdn.apache.org/maven/maven-3/3.9.2/binaries/apache-maven-3.9.2-bin.tar.gz | gzip -dc | tar -C $HOME/.local/java -xf -
 curl -L https://services.gradle.org/distributions/gradle-8.1.1-bin.zip > $HOME/tmp/gradle-bin.zip
-unzip $HOME/tmp/gradle-bin.zip -d $HOME/lang/java
-ln -s $HOME/lang/java/amazon-corretto-* $HOME/lang/java/jdk
-ln -s $HOME/lang/java/apache-maven-* $HOME/lang/java/apache-maven
-ln -s $HOME/lang/java/gradle-* $HOME/lang/java/gradle
+unzip $HOME/tmp/gradle-bin.zip -d $HOME/.local/java
+ln -s $HOME/.local/java/amazon-corretto-* $HOME/lang/.local/jdk
+ln -s $HOME/.local/java/apache-maven-* $HOME/lang/.local/apache-maven
+ln -s $HOME/.local/java/gradle-* $HOME/lang/.local/gradle
 rm -f $HOME/tmp/gradle-bin.zip
 
