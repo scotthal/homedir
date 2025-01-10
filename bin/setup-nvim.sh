9855d4e4c939ee34a432b48343aab371299b85f8
Scott Hallock <scott@hallocks.net>
2025-01-10 10:33:23 -0700

    Move neovim to ~/.local

--- bin/setup-nvim.sh
+++ bin/setup-nvim.sh
@@ -1,4 +1,4 @@
-mkdir -p /opt
-curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | gzip -dc | tar -C /opt -xf -
-mv /opt/nvim-linux64 /opt/nvim
+mkdir -p $HOME/.local
+curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | gzip -dc | tar -C $HOME/.local -xf -
+mv $HOME/.local/nvim-linux64 $HOME/.local/nvim
 
