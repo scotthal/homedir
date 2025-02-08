840ba15a91f649d24feba6325c197266972288a5
Scott Hallock <scott@hallocks.net>
2025-02-08 12:06:01 -0700

    Add 'node' hardlink
    This allows many scripts to work.

--- bin/setup-bun.sh
+++ bin/setup-bun.sh
@@ -4,5 +4,6 @@ cd $HOME/tmp
 curl -LO https://github.com/oven-sh/bun/releases/latest/download/bun-linux-x64.zip
 unzip bun-linux-x64.zip
 install bun-linux-x64/bun $HOME/.local/bin/bun
+ln $HOME/.local/bin/bun $HOME/.local/bin/node
 rm -rf bun-linux-x64.zip bun-linux-x64
 
