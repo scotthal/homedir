38f5fa45f67807f10fd0f8b67652faf8448f264c
Scott Hallock <scott@hallocks.net>
2025-08-09 17:23:29 -0600

    Install emmet-language-server when installing bun

--- bin/setup-bun.sh
+++ bin/setup-bun.sh
@@ -7,4 +7,5 @@ install bun-linux-x64/bun $HOME/.local/bin/bun
 ln $HOME/.local/bin/bun $HOME/.local/bin/node
 rm -rf bun-linux-x64.zip bun-linux-x64
 $HOME/.local/bin/bun install -g vscode-langservers-extracted
+$HOME/.local/bin/bun install -g @olrtg/emmet-language-server
 
